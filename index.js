a8f913cff10096d000fc8464ba6f236ce6ddd748
bendobos <bendobos1@gmail.com>
2017-05-18 11:17:16 +0200

    Add metalsmith-watch in dev environment

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ var contentful   = require('contentful-metalsmith');
 var permalinks   = require('metalsmith-permalinks');
 var sass         = require('metalsmith-sass');
 var assets       = require('metalsmith-assets');
+var msIf         = require('metalsmith-if');
+var watch        = require('metalsmith-watch');
+var watcher      = false;
 var datamarkdown = require('metalsmith-data-markdown');
+
+if (process.env.NODE_ENV !== 'production') {
+  var watcher = true
+}
 
 Metalsmith(__dirname)
   .metadata({
@@ -17,6 +24,16 @@ Metalsmith(__dirname)
   .source('./src')
   .destination('./build')
   .clean(true)
+  .use(msIf(
+    watcher,
+    watch({
+      paths: {
+        "${source}/**/*": true,
+        "layouts/**/*": "**/*.md",
+        "partials/**/*": "**/*.md"
+      }
+    })
+  ))
   .use(contentful({
     'access_token': '48f2dd2572a725053eaa6e8455a6be9fa0c49b3fdb54beabbb515dfafe70a764',
     'space_id': 'uyzbu4k0mvui'
