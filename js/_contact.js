8a03113eb7eb6759a0fecc9ec650e33ef2b20e0d
bendobos <bendobos1@gmail.com>
2017-06-04 13:03:48 +0200

    Disable form submission on pressing enter

--- js/_contact.js
+++ js/_contact.js
@@ -10,3 +10,11 @@ $("#contact-form").submit(function(e) {
     $(".success").toggleClass("hidden");
   });
 });
+
+$('#contact-form').on('keyup keypress', function(e) {
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === 13) {
+    e.preventDefault();
+    return false;
+  }
+});
